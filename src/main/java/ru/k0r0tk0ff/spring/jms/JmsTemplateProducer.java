e61fa443a5ef05ae9457a7f9ca437a1a31f48de5
Alexandr <korotkov34reg@gmail.com>
2018-09-13 09:46:42 +0300

    del unuseful lib

--- src/main/java/ru/k0r0tk0ff/spring/jms/JmsTemplateProducer.java
+++ src/main/java/ru/k0r0tk0ff/spring/jms/JmsTemplateProducer.java
@@ -4,12 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
 import javax.jms.TextMessage;
 
 @Component
