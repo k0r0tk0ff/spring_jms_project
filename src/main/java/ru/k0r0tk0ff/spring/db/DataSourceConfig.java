f157c7e9074a3e93418e54490009f6fbbb34e700
Alexandr <korotkov34reg@gmail.com>
2018-09-13 09:38:10 +0300

    Create db/DataSourceConfig.java

---
+++ src/main/java/ru/k0r0tk0ff/spring/db/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package ru.k0r0tk0ff.spring.db;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@PropertySources({ @PropertySource("classpath:datasource-cfg.properties") })
+public class DataSourceConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+}
